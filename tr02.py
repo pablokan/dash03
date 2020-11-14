1de96aecb3ba41e3979c3c3f7c1122bbf3a588ad
pablokan <pablokan@gmail.com>
2020-11-13 22:29:30 -0300

    dynamic update

--- tr02.py
+++ tr02.py
@@ -8,10 +8,12 @@ import pandas as pd
 # reading the database
 def readDB():
     import pymysql
-    db = pymysql.connect("localhost", "root", "root", "tecnoredDB" )
+    db = pymysql.connect("161.35.103.252", "tecnored", "tecno2020$Red", "tecnoredDB" )
     cur = db.cursor()
-    cur.execute("SELECT FOS, TAC, FOSdivTAC, fech, dige_id FROM charDige")
+    cur.execute("SELECT FOS, TAC, FOS/TAC, fech, dige_id FROM charDige")
     return cur.fetchall()
+    db.close()
+
 datos = readDB()
 
 # instancia la app de Dash y mete estilos
@@ -19,11 +21,11 @@ external_stylesheets = ['bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # construye el dataframe con Pandas 
-df = pd.DataFrame.from_records(datos, columns=["FOS", "TAC", "FOSdivTAC", "Fecha", "dige_id"])
+df = pd.DataFrame.from_records(datos, columns=["FOS", "TAC", "FOS/TAC", "Fecha", "dige_id"])
 
 # arma el layout
 app.layout = html.Div([
-    html.H1(children='Digestor', style={'textAlign': 'center'}),
+    html.H1(children='Digestores', style={'textAlign': 'center'}),
     html.Div([
         html.Div([
             dcc.Dropdown(
@@ -35,8 +37,13 @@ app.layout = html.Div([
         style={'width': '20%',  'margin': '0 auto'}),
     ]),
     html.Div([
-    html.Div(
+    html.Div([
         dcc.Graph(id='g1'),
+        dcc.Interval(
+            id='interval-component',
+            interval=1000, # in milliseconds
+            n_intervals=0
+        )],
         style={'width': '48%', 'display': 'inline-block'}
     ),
     html.Div(
@@ -54,19 +61,22 @@ app.layout = html.Div([
         [Output('g1', 'figure'),
         Output('g2', 'figure'),
         Output('g3', 'figure')],
-        [Input('comboDigestores', 'value')]
+        [Input('comboDigestores', 'value'),
+        Input('interval-component', 'n_intervals')]
         )
     
-def update_figure(comboDigestores):
+def update_figure(comboDigestores, interval_component):
+    datos = readDB()
+    df = pd.DataFrame.from_records(datos, columns=["FOS", "TAC", "FOS/TAC", "Fecha", "dige_id"])
     filDig = 1
     if comboDigestores == "Principal":
         filDig = 1
     elif comboDigestores == "Secundario":
         filDig = 2
     filtered_df = df[df.dige_id == filDig]
-    fig1 = px.area(filtered_df, x="Fecha", y="FOS", color_discrete_sequence=["red"])
+    fig1 = px.area(filtered_df, x="Fecha", y="FOS", color_discrete_sequence=["yellow"])
     fig2 = px.area(filtered_df, x="Fecha", y="TAC", color_discrete_sequence=["blue"])
-    fig3 = px.area(filtered_df, x="Fecha", y="FOSdivTAC", color_discrete_sequence=["green"])
+    fig3 = px.area(filtered_df, x="Fecha", y="FOS/TAC", color_discrete_sequence=["green"])
     fig1.update_layout(transition_duration=500)
     fig2.update_layout(transition_duration=500)
     fig3.update_layout(transition_duration=500)
